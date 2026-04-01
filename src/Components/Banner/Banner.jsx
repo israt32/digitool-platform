f360f32245c0e70200a0cea22dadaaa2d54bcfb6
Israt Jahan <israt5227@gmail.com>
2026-04-01 19:57:08 +0600

    finished banner section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,32 +4,40 @@ import circleImg from "../../assets/products/Group 5.png";
 import { CiPlay1 } from "react-icons/ci";
 const Banner = () => {
   return (
-    <div className="container mx-auto">
-      <div>
-        <span className="p-4 rounded-full text-[16px] font-medium badge badge-xs badge-warning bg-[#E1E7FF] border-none text-[#9514FA]">
-          <span>
-            <img src={circleImg} alt="" />
+   
+      <div className="flex gap-14 justify-around container mx-auto py-20 items-center">
+        <div className="w-3/5">
+          <span className="p-4 rounded-full text-[16px] font-medium badge badge-xs badge-warning bg-[#E1E7FF] border-none text-[#9514FA] mb-6">
+            <span>
+              <img src={circleImg} alt="" />
+            </span>
+            New: AI-Powered Tools Available
           </span>
-          New: AI-Powered Tools Available
-        </span>
-        <h1 className="font-extrabold text-7xl opacity-90">
-          Supercharge Your <br />
-          Digital Workflow
-        </h1>
-        <p className="text-[#627382] text-[18px]">
-          Access premium AI tools, design assets, templates, and productivity
-          software—all in one place. Start creating faster today. Explore
-          Products
-        </p>
-        <div>
-          <button className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white font-bold rounded-full p-5">Explore Products</button>
-          <button className="btn p-5 text-[#9514FA] border border-[#9514FA] rounded-full "><CiPlay1 /> Watch Demo</button>
+          <h1 className="font-extrabold text-7xl opacity-90 leading-21 mb-4">
+            Supercharge Your <br />
+            Digital Workflow
+          </h1>
+          <p className="text-[#627382] text-[18px] mb-8">
+            Access premium AI tools, design assets, templates, and productivity <br />
+            software—all in one place. Start creating faster today. Explore
+            Products
+          </p>
+          <div >
+            <button className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white font-bold rounded-full p-5 mr-4">
+              Explore Products
+            </button>
+            <button className="btn p-5 text-[#9514FA] border border-[#9514FA] rounded-full ">
+              <CiPlay1 /> Watch Demo
+            </button>
+          </div>
         </div>
+        <div className="w-2/5">
+          <img className="w-full" src={bannerImg} alt="" />
+
+        </div>
+        
       </div>
-      <div>
-        <img src={bannerImg} alt="" />
-      </div>
-    </div>
+    
   );
 };
 
